c5822a20684fb19968bab982e2b0aaad432d7dfb
DaxtonAltarus <84556668+DaxtonAltarus@users.noreply.github.com>
2022-03-08 04:53:04 -0600

    Primera app

---
+++ src/Inicio.js
@@ -0,0 +1,9 @@
+function Inicio(){
+    return(
+        <div>
+            <h1>¡Bienvenido a React!</h1>
+            <p>Está a punto de iniciar un emocionante viaje en el que creará aplicacones web usando las tecnologías más recientes</p>
+      </div>
+    )
+}
+export default Inicio;
